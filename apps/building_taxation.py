25d28a958e5ee17b7ca3de772caeac73d721636f
hsa <heming.aldrin@proton.me>
2026-05-25 15:27:43 +0200

    split up plots + make more functions not depend on global variables

--- apps/building_taxation.py
+++ apps/building_taxation.py
@@ -37,7 +37,7 @@ def _():
 
 
 @app.cell
-def _(create_add_remove_buttons):
+def _(MAX_SCENARIOS, MIN_SCENARIOS):
     default_sandbox_vals = {
         "tier1_rate": 25.0,
         "tier2_rate": 70.0,
@@ -53,7 +53,9 @@ def _(create_add_remove_buttons):
         set_visible_count,
     ) = create_scenario_manager(default_sandbox_vals)
 
-    add_button, remove_button = create_add_remove_buttons(set_visible_count)
+    add_button, remove_button = create_add_remove_buttons(
+        set_visible_count, MIN_SCENARIOS, MAX_SCENARIOS, mo
+    )
     return (
         add_button,
         get_scenarios,
@@ -132,13 +134,13 @@ def _(is_couple, mortgage_debt, other_net_wealth, ui_sliders):
 
 @app.cell
 def _(
+    COLORS,
     MAX_SCENARIOS,
     MIN_SCENARIOS,
     add_button,
     alternatives,
     get_visible_count,
     remove_button,
-    render_scenario_sliders,
 ):
     ui_sliders = create_slider_ui(
         alternatives,
@@ -152,6 +154,7 @@ def _(
                 "base_deduction",
                 "tax_rate",
             ],
+            COLORS,
             mo,
         ),
         get_visible_count,
@@ -171,13 +174,16 @@ def _(ui_elements):
 
 
 @app.cell
-def _(create_charts, tax_df):
-    create_charts(tax_df)
+def _(COLORS, tax_df):
+    val_chart = create_valuation_chart(tax_df, COLORS, get_chart_domain_and_range)
+    tax_chart = create_tax_chart(tax_df, COLORS, get_chart_domain_and_range)
+
+    mo.vstack([val_chart, tax_chart])
     return
 
 
 @app.cell
-def _(alternatives, calculate_wealth_tax_df):
+def _(alternatives, is_couple, mortgage_debt, other_net_wealth):
     current_df = calculate_wealth_tax_df(
         tier1_rate=25.0,
         tier2_rate=70.0,
@@ -185,140 +191,150 @@ def _(alternatives, calculate_wealth_tax_df):
         base_deduction=1_900_000.0,
         tax_rate=1.0,
         scenario_name="Dagens regelverk",
+        is_couple=is_couple.value,
+        mortgage_debt=mortgage_debt.value,
+        other_net_wealth=other_net_wealth.value,
     )
 
-    df_alts = build_alternatives(alternatives, calculate_wealth_tax_df)
+    df_alts = build_alternatives(
+        alternatives,
+        calculate_wealth_tax_df,
+        is_couple=is_couple.value,
+        mortgage_debt=mortgage_debt.value,
+        other_net_wealth=other_net_wealth.value,
+    )
     tax_df = pl.concat([current_df] + df_alts)
     return (tax_df,)
 
 
-@app.cell
-def _(is_couple, mortgage_debt, other_net_wealth):
-    def calculate_wealth_tax_df(
-        tier1_rate: float,
-        tier2_rate: float,
-        valuation_threshold: float,
-        base_deduction: float,
-        tax_rate: float,
-        scenario_name: str,
-    ) -> pl.DataFrame:
-        market_values = pl.Series("market_value", range(0, 30_500_000, 500_000))
-        df = pl.DataFrame([market_values])
+@app.function
+def calculate_wealth_tax_df(
+    tier1_rate: float,
+    tier2_rate: float,
+    valuation_threshold: float,
+    base_deduction: float,
+    tax_rate: float,
+    scenario_name: str,
+    is_couple: bool,
+    mortgage_debt: float,
+    other_net_wealth: float,
+) -> pl.DataFrame:
+    market_values = pl.Series("market_value", range(0, 30_500_000, 500_000))
+    df = pl.DataFrame([market_values])
 
+    df = df.with_columns(
+        valuation=pl.when(pl.col("market_value") <= valuation_threshold)
+        .then(pl.col("market_value") * (tier1_rate / 100))
+        .otherwise(
+            valuation_threshold * (tier1_rate / 100)
+            + (pl.col("market_value") - valuation_threshold) * (tier2_rate / 100)
+        )
+    )
+
+    df = df.with_columns(
+        net_wealth=pl.col("valuation") + other_net_wealth - mortgage_debt
+    )
+
+    actual_base_ded = base_deduction * 2 if is_couple else base_deduction
+
+    df = df.with_columns(
+        taxable_wealth=pl.max_horizontal(0, pl.col("net_wealth") - actual_base_ded)
+    )
+
+    if scenario_name == "Dagens regelverk":
         df = df.with_columns(
-            valuation=pl.when(pl.col("market_value") <= valuation_threshold)
-            .then(pl.col("market_value") * (tier1_rate / 100))
+            tax=pl.when(pl.col("taxable_wealth") <= 20_000_000)
+            .then(pl.col("taxable_wealth") * 0.01)
             .otherwise(
-                valuation_threshold * (tier1_rate / 100)
-                + (pl.col("market_value") - valuation_threshold) * (tier2_rate / 100)
+                20_000_000 * 0.01 + (pl.col("taxable_wealth") - 20_000_000) * 0.011
             )
         )
+    else:
+        df = df.with_columns(tax=pl.col("taxable_wealth") * (tax_rate / 100))
 
-        df = df.with_columns(
-            net_wealth=pl.col("valuation") + other_net_wealth.value - mortgage_debt.value
-        )
-
-        actual_base_ded = base_deduction * 2 if is_couple.value else base_deduction
-
-        df = df.with_columns(
-            taxable_wealth=pl.max_horizontal(0, pl.col("net_wealth") - actual_base_ded)
-        )
-
-        if scenario_name == "Dagens regelverk":
-            df = df.with_columns(
-                tax=pl.when(pl.col("taxable_wealth") <= 20_000_000)
-                .then(pl.col("taxable_wealth") * 0.01)
-                .otherwise(
-                    20_000_000 * 0.01 + (pl.col("taxable_wealth") - 20_000_000) * 0.011
-                )
-            )
-        else:
-            df = df.with_columns(tax=pl.col("taxable_wealth") * (tax_rate / 100))
-
-        df = df.with_columns(pl.lit(scenario_name).alias("Scenario"))
-        return df
-
-    return (calculate_wealth_tax_df,)
+    df = df.with_columns(pl.lit(scenario_name).alias("Scenario"))
+    return df
 
 
 @app.function
-def build_alternatives(alts, calc_fn):
+def build_alternatives(alts, calc_fn, is_couple, mortgage_debt, other_net_wealth):
     df_alternatives = []
     for i, _alternative in enumerate(alts):
         kwargs = {key: _alternative[key].value for key in _alternative}
-        df = calc_fn(**kwargs, scenario_name=f"Alternativ {i + 1}")
+        df = calc_fn(
+            **kwargs,
+            scenario_name=f"Alternativ {i + 1}",
+            is_couple=is_couple,
+            mortgage_debt=mortgage_debt,
+            other_net_wealth=other_net_wealth,
+        )
         df_alternatives.append(df)
     return df_alternatives
 
 
-@app.cell
-def _(COLORS):
-    def create_charts(df):
-        num_alts = df["Scenario"].n_unique() - 1
-        domain = ["Dagens regelverk"] + [f"Alternativ {i + 1}" for i in range(num_alts)]
-        range_ = ["#000000"] + COLORS[:num_alts]
-
-        val_chart = (
-            alt.Chart(df)
-            .mark_line()
-            .encode(
-                x=alt.X("market_value:Q", title="Reell markedsverdi (NOK)"),
-                y=alt.Y("valuation:Q", title="Formuesverdi (NOK)"),
-                color=alt.Color(
-                    "Scenario:N",
-                    scale=alt.Scale(
-                        domain=domain,
-                        range=range_,
-                    ),
-                ),
-                tooltip=["market_value", "valuation", "Scenario"],
-            )
-            .properties(
-                width="container", height=350, title="Verdsettelseskurve (Formuesverdi)"
-            )
-        )
-
-        tax_chart = (
-            alt.Chart(df)
-            .mark_line()
-            .encode(
-                x=alt.X("market_value:Q", title="Reell markedsverdi (NOK)"),
-                y=alt.Y("tax:Q", title="Årlig formuesskatt (NOK)"),
-                color=alt.Color(
-                    "Scenario:N",
-                    scale=alt.Scale(
-                        domain=domain,
-                        range=range_,
-                    ),
-                ),
-                tooltip=["market_value", "tax", "Scenario"],
-            )
-            .properties(width="container", height=350, title="Formuesskatteffekt")
-        )
-
-        return mo.vstack([val_chart, tax_chart])
-
-    return (create_charts,)
+@app.function
+def get_chart_domain_and_range(df, colors):
+    num_alts = df["Scenario"].n_unique() - 1
+    domain = ["Dagens regelverk"] + [f"Alternativ {i + 1}" for i in range(num_alts)]
+    range_ = ["#000000"] + colors[:num_alts]
+    return domain, range_
 
 
-@app.cell
-def _(MAX_SCENARIOS, MIN_SCENARIOS):
-    def create_add_remove_buttons(set_visible_count_fn):
-        add_button = mo.ui.button(
-            label="Legg til alternativ",
-            on_change=lambda _: set_visible_count_fn(
-                lambda count: min(count + 1, MAX_SCENARIOS)
+@app.function
+def create_valuation_chart(df, colors, get_chart_domain_and_range_fn):
+    domain, range_ = get_chart_domain_and_range_fn(df, colors)
+    return (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x=alt.X("market_value:Q", title="Reell markedsverdi (NOK)"),
+            y=alt.Y("valuation:Q", title="Formuesverdi (NOK)"),
+            color=alt.Color(
+                "Scenario:N",
+                scale=alt.Scale(domain=domain, range=range_),
             ),
+            tooltip=["market_value", "valuation", "Scenario"],
         )
-        remove_button = mo.ui.button(
-            label="Fjern alternativ",
-            on_change=lambda _: set_visible_count_fn(
-                lambda count: max(count - 1, MIN_SCENARIOS)
-            ),
+        .properties(
+            width="container", height=350, title="Verdsettelseskurve (Formuesverdi)"
         )
-        return add_button, remove_button
+    )
 
-    return (create_add_remove_buttons,)
+
+@app.function
+def create_tax_chart(df, colors, get_chart_domain_and_range_fn):
+    domain, range_ = get_chart_domain_and_range_fn(df, colors)
+    return (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x=alt.X("market_value:Q", title="Reell markedsverdi (NOK)"),
+            y=alt.Y("tax:Q", title="Årlig formuesskatt (NOK)"),
+            color=alt.Color(
+                "Scenario:N",
+                scale=alt.Scale(domain=domain, range=range_),
+            ),
+            tooltip=["market_value", "tax", "Scenario"],
+        )
+        .properties(width="container", height=350, title="Formuesskatteffekt")
+    )
+
+
+@app.function
+def create_add_remove_buttons(set_visible_count_fn, min_scenarios, max_scenarios, mo):
+    add_button = mo.ui.button(
+        label="Legg til alternativ",
+        on_change=lambda _: set_visible_count_fn(
+            lambda count: min(count + 1, max_scenarios)
+        ),
+    )
+    remove_button = mo.ui.button(
+        label="Fjern alternativ",
+        on_change=lambda _: set_visible_count_fn(
+            lambda count: max(count - 1, min_scenarios)
+        ),
+    )
+    return add_button, remove_button
 
 
 @app.function
@@ -328,21 +344,18 @@ def create_scenario_manager(default_values, max_scenarios=4):
     return get_scenarios, set_scenarios, get_visible_count, set_visible_count
 
 
-@app.cell
-def _(COLORS):
-    def render_scenario_sliders(scenario_dict, color_index, keys, mo):
-        color = COLORS[color_index % len(COLORS)]
-        rendered_sliders = mo.hstack([scenario_dict[key] for key in keys])
-        colored_sliders = mo.vstack([rendered_sliders]).style(
-            {
-                "border-left": f"4px solid {color}",
-                "padding-left": "10px",
-                "margin": "10px 0",
-            }
-        )
-        return colored_sliders
-
-    return (render_scenario_sliders,)
+@app.function
+def render_scenario_sliders(scenario_dict, color_index, keys, colors, mo):
+    color = colors[color_index % len(colors)]
+    rendered_sliders = mo.hstack([scenario_dict[key] for key in keys])
+    colored_sliders = mo.vstack([rendered_sliders]).style(
+        {
+            "border-left": f"4px solid {color}",
+            "padding-left": "10px",
+            "margin": "10px 0",
+        }
+    )
+    return colored_sliders
 
 
 @app.function
